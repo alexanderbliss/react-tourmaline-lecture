2e821d097bf6dda84f30b8ec0a2b2d135d07d912
Alex Bliss <alexander.blss@gmail.com>
2023-08-09 20:35:27 -0500

    end of lecture commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from '../Header/header'
+import Panda from '../Panda/Panda'
+
+
+
+// function greet(name, shoeColor){
+//    return `Hello ${name}! Nice ${shoeColor} Shoes!`
+//   }  
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+
+      <Header />
+
+
+    <Panda disposition="friendly" name="Pandoot" />
+    <Panda disposition="fluffy" name="Floof" />
+    <Panda disposition="tiny" name="Lil' Paws" />
+    <Panda disposition="dreamy" name="Snooze" />
+    <Panda disposition="uniquely wonderful" name="Kris Szafranski" />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Panda/Panda.jsx
@@ -0,0 +1,13 @@
+function Panda(props){
+    return (
+        <h3> Hey. I'm a {props.disposition} Panda! 🐼 My name is {props.name}. </h3>
+    )
+}
+
+
+// Panda({
+//disposition: freindly,
+//name: Pandoot
+//})
+
+export default Panda;
